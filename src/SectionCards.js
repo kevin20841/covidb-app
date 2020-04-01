217527a07ede22dc11ecaa0031fcef060e9f7832
natel9178 <natel9178@gmail.com>
2020-03-31 23:41:13 -0400

    formatting

--- src/SectionCards.js
+++ src/SectionCards.js
@@ -1,49 +1,92 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import SectionCard from "./SectionCard"
-import Grid from '@material-ui/core/Grid';
+import { makeStyles } from "@material-ui/core/styles";
+import SectionCard from "./SectionCard";
+import Grid from "@material-ui/core/Grid";
 
 import Container from "@material-ui/core/Container";
 
 //This will be inherited eventually
-let sectionTitles = [["Aggregated Resources on COVID-19", "Health And Hygiene"], ["Metrics And Trackers", "Fact-checking Sites"],
- ["Countries", "Legal"], ["Simulations/Projections", "Historical Pandemics"]];
+let sectionTitles = [
+  ["Aggregated Resources on COVID-19", "Health And Hygiene"],
+  ["Metrics And Trackers", "Fact-checking Sites"],
+  ["Countries", "Legal"],
+  ["Simulations/Projections", "Historical Pandemics"]
+];
 
- let sectionCategories = {
-  "Aggregated Resources on COVID-19": ["Sub Category 1", "Sub Category 2", "Sub Category 3", "Sub Category 4"],
-  "Health And Hygiene": ["Sub Category 1", "Sub Category 2", "Sub Category 3", "Sub Category 4"],
-  "Metrics And Trackers": ["Sub Category 1", "Sub Category 2", "Sub Category 3", "Sub Category 4"],
-  "Fact-checking Sites": ["Sub Category 1", "Sub Category 2", "Sub Category 3", "Sub Category 4"],
-  "Countries":["Sub Category 1", "Sub Category 2", "Sub Category 3", "Sub Category 4"],
-  "Legal":["Sub Category 1", "Sub Category 2", "Sub Category 3", "Sub Category 4"],
-  "Simulations/Projections":["Sub Category 1", "Sub Category 2", "Sub Category 3", "Sub Category 4"],
-  "Historical Pandemics":["Sub Category 7", "Sub Category 2", "Sub Category 3", "Sub Category 4"],
- };
+let sectionCategories = {
+  "Aggregated Resources on COVID-19": [
+    "Sub Category 1",
+    "Sub Category 2",
+    "Sub Category 3",
+    "Sub Category 4"
+  ],
+  "Health And Hygiene": [
+    "Sub Category 1",
+    "Sub Category 2",
+    "Sub Category 3",
+    "Sub Category 4"
+  ],
+  "Metrics And Trackers": [
+    "Sub Category 1",
+    "Sub Category 2",
+    "Sub Category 3",
+    "Sub Category 4"
+  ],
+  "Fact-checking Sites": [
+    "Sub Category 1",
+    "Sub Category 2",
+    "Sub Category 3",
+    "Sub Category 4"
+  ],
+  Countries: [
+    "Sub Category 1",
+    "Sub Category 2",
+    "Sub Category 3",
+    "Sub Category 4"
+  ],
+  Legal: [
+    "Sub Category 1",
+    "Sub Category 2",
+    "Sub Category 3",
+    "Sub Category 4"
+  ],
+  "Simulations/Projections": [
+    "Sub Category 1",
+    "Sub Category 2",
+    "Sub Category 3",
+    "Sub Category 4"
+  ],
+  "Historical Pandemics": [
+    "Sub Category 7",
+    "Sub Category 2",
+    "Sub Category 3",
+    "Sub Category 4"
+  ]
+};
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: 'white',
-    marginBottom: '80px',
-    backgroundColor: 'white',
+    backgroundColor: "white",
+    marginBottom: "80px",
+    backgroundColor: "white"
   },
   paper: {
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    textAlign: "center",
+    color: theme.palette.text.secondary
   },
-  gridItem:{
-    display: 'block',
-    marginLeft: '0px',
+  gridItem: {
+    display: "block",
+    marginLeft: "0px"
   },
-  gridItemRight:{
-    display: 'block',
-    paddingLeft: '50px',
+  gridItemRight: {
+    display: "block",
+    paddingLeft: "50px"
   },
-  row:{
-  },
+  row: {},
   grid: {
-    alignContent: 'space-between'
+    alignContent: "space-between"
   }
 }));
 
@@ -54,12 +97,18 @@ export default function SectionCards() {
     return (
       <React.Fragment className={classes.row}>
         <Grid item xs={6} className={classes.gridItem}>
-          <SectionCard titleText={props.items[0]} subcategoryList={props.subcategories[props.items[0]]}/>
+          <SectionCard
+            titleText={props.items[0]}
+            subcategoryList={props.subcategories[props.items[0]]}
+          />
         </Grid>
         <Grid item xs={6} className={classes.gridItemRight}>
-          <SectionCard titleText={props.items[1]} subcategoryList={props.subcategories[props.items[1]]} isRight={true} />
+          <SectionCard
+            titleText={props.items[1]}
+            subcategoryList={props.subcategories[props.items[1]]}
+            isRight={true}
+          />
         </Grid>
-
       </React.Fragment>
     );
   }
@@ -67,16 +116,20 @@ export default function SectionCards() {
   return (
     <div className={classes.root}>
       <Container maxWidth="md">
-
         <Grid container spacing={1} className={classes.grid}>
-
-        {sectionTitles.map((value)=> {
-          return <Grid container item xs={12} spacing={3} key={value}>
-            <FormRow items={value} key={value} subcategories={sectionCategories}/>
-          </Grid>
-        })}
-      </Grid>
-    </Container>
-  </div>
+          {sectionTitles.map(value => {
+            return (
+              <Grid container item xs={12} spacing={3} key={value}>
+                <FormRow
+                  items={value}
+                  key={value}
+                  subcategories={sectionCategories}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    </div>
   );
 }
